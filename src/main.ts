a67bd0b1cd51a29df19d1c5a58e4036349bf96d6
Carolayne <carolaynepenha@gmail.com>
2024-09-01 18:27:36 -0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,6 @@ import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(json({ limit: '50mb' }));
-  app.use(urlencoded({ extended: true, limit: '50mb' }));
   const port = process.env.ACCESS_PORT || 5500;
   await app.listen(port, () => {
     console.log(`Server is up and running on port ${port}`);
